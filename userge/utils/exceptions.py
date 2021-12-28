181db9ecce8ee07a2f72eb34543e7d4e544ba53b
priest9680 <60083597+priest9680@users.noreply.github.com>
2021-12-28 12:29:18 +0530

    Update exceptions.py

--- userge/utils/exceptions.py
+++ userge/utils/exceptions.py
@@ -10,12 +10,12 @@
 
 
 class StopConversation(Exception):
-    """raise if conversation has terminated"""
+    """Raise if conversation has terminated"""
 
 
 class ProcessCanceled(Exception):
-    """raise if thread has terminated"""
+    """Raise if thread has terminated"""
 
 
 class UsergeBotNotFound(Exception):
-    """raise if userge bot not found"""
+    """Raise if userge bot not found"""
